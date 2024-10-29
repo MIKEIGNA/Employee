f350c53058c9e86acec8d1094d4f1485b0943b40
MIKEIGNA <entsudy@gmail.com>
2024-10-29 16:58:37 +0300

    modified

--- server/models/user.js
+++ server/models/user.js
@@ -1,25 +1,47 @@
-// models/User.js
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    fullName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    idNumber: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    accountNumber: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+// // models/User.js
+// module.exports = (sequelize, DataTypes) => {
+//   const User = sequelize.define('User', {
+//     fullName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     idNumber: {
+//       type: DataTypes.STRING,
+//       unique: true,
+//       allowNull: false,
+//     },
+//     accountNumber: {
+//       type: DataTypes.STRING,
+//       unique: true,
+//       allowNull: false,
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//   });
 
-  return User;
-};
+//   return User;
+// };
+
+// server/models/User.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class User extends Model {}
+
+User.init({
+  fullName: DataTypes.STRING,
+  idNumber: DataTypes.STRING,
+  accountNumber: DataTypes.STRING,
+  password: DataTypes.STRING,
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'user', // Default role is 'user'
+  }
+}, {
+  sequelize,
+  modelName: 'User',
+});
+
+module.exports = User;
---
+++ server/seeders/xxxxxx-admin-user.js
@@ -0,0 +1,23 @@
+// seeds/xxxxxx-admin-user.js
+'use strict';
+const bcrypt = require('bcryptjs'); // Import bcrypt
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      fullName: 'Admin User',
+      idNumber: 'ADMIN123',
+      accountNumber: 'ADMIN_ACCOUNT',
+      password: await bcrypt.hash('AdminPassword', 10), // Hash the password
+      role: 'admin',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', {
+      accountNumber: 'ADMIN_ACCOUNT'
+    });
+  }
+};
