81510c826e1cb6d590e8d9b32066046648f3a067
MIKEIGNA <entsudy@gmail.com>
2024-10-29 16:18:03 +0300

    clean

--- client_code/src/App.jsx
+++ client_code/src/App.jsx
@@ -1,7 +1,5 @@
 // client_code/src/App.jsx
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,16 +7,6 @@ import Register from './pages/Register';
 import './scss/main.scss';
 
 function App() {
-  // return (
-  //   <Router>
-  //     <Navbar />
-  //     <Routes>
-  //       <Route path="/" element={<Home />} /> |
-  //       <Route path="/login" element={<Login />} /> | 
-  //       <Route path="/register" element={<Register />} />
-  //     </Routes>
-  //   </Router>
-  // );
   return (
     <Router>
         <div className="App">
--- client_code/src/pages/Home.jsx
+++ client_code/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 // client_code/src/pages/Home.jsx
 const Home = () => (
     <div className="home" style={{ textAlign: 'center' }}>
-      <h2>Welcome to the International Payment Portal</h2>
+      <h2>Welcome to the International Customer Portal</h2>
     </div>
 );
   
